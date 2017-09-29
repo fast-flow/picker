d2e1edba7c42b664a497c8f1d391f547b92e06f1
Nimo <nimo.jser@gmail.com>
2017-09-29 10:06:56 +0800

    PickerView

--- example/basic.demo.js
+++ example/basic.demo.js
@@ -1,25 +1,53 @@
 var React = require('react')
 var Picker = require('picker.react').default
+var PickerView = require('picker.react').PickerView
 var App = React.createClass({
     getInitialState: function () {
         return {
-            show: true
+
         }
     },
     render: function () {
         var self = this
         return (
             <div>
-                {
-                    self.state.show?
-                    (
-                        <Picker onClose={function() {
-                                self.setState({
-                                    show: false
-                                })
-                            }} >basic</Picker>
-                    ):null
-                }
+                <PickerView
+                    data={[
+                        {
+                            label: 'a',
+                            value: '1'
+                        },
+                        {
+                            label: 'b',
+                            value: '2'
+                        },
+                        {
+                            label: 'c',
+                            value: '3'
+                        },
+                        {
+                            label: 'd',
+                            value: '4'
+                        },
+                        {
+                            label: 'e',
+                            value: '5'
+                        },
+                        {
+                            label: 'f',
+                            value: '6'
+                        },
+                        {
+                            label: 'g',
+                            value: '7'
+                        }
+                        ,
+                        {
+                            label: 'h',
+                            value: '8'
+                        }
+                    ]}
+                />
             </div>
         )
     }
